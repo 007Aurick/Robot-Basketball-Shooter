b52543ff164230f7ac39bef3d944e7fc22fb5730
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-08-19 02:16:39 -0400

    Readded Proper File

--- mujoco_menagerie/play_around.py
+++ mujoco_menagerie/play_around.py
@@ -9,19 +9,15 @@ scene = Path(__file__).resolve().parent / "unitree_g1" / "scene.xml"
 model = mujoco.MjModel.from_xml_path(str(scene))
 data = mujoco.MjData(model)
 
-# Start from stand pose
-key_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_KEY, "stand")
-mujoco.mj_resetDataKeyframe(model, data, key_id)
-stand_ctrl = data.ctrl.copy()
-
 wrist = model.actuator("left_wrist_pitch_joint").id
-freq = 2.0
+wrist2 = model.actuator("right_wrist_pitch_joint").id
+
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     t = 0.0
     while viewer.is_running():
-        data.ctrl[:] = stand_ctrl
-        data.ctrl[wrist] = 0.5 * np.sin(2 * np.pi * freq * t)
+        data.ctrl[wrist] = 0.5 * np.sin(t)
+        data.ctrl[wrist2] = 0.5 * np.sin(t)
         mujoco.mj_step(model, data)
         viewer.sync()
         t += model.opt.timestep
